40a85b4f2986e2d5baa38f7b3b8bf88c5a35cd3b
stadnikwhitmeyer529 <stadnikwhitmeyer529@gmail.com>
2025-03-11 17:12:31 +0000

    Add 6.go

---
+++ src/6.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare variables
+	var num1 int = 20
+	var num2 int = 30
+
+	// Calculate sum and difference
+	sum := num1 + num2
+	diff := num1 - num2
+
+	// Display results
+	fmt.Println("Sum:", sum)
+	fmt.Println("Difference:", diff)
+}

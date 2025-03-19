597eb92a20f73535f9879900c84224d3b2a0b1a3
stadnikwhitmeyer529 <stadnikwhitmeyer529@gmail.com>
2025-03-19 04:31:57 +0000

    Add 13.go

---
+++ src/13.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(100) + 1
+	fmt.Println("The random number is:", n)
+}

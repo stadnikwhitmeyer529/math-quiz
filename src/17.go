007c5fc6f107db182d027d2d7fc62a6dd5622ecf
stadnikwhitmeyer529 <stadnikwhitmeyer529@gmail.com>
2025-03-24 21:18:52 +0000

    Add 17.go

---
+++ src/17.go
@@ -0,0 +1,11 @@
+def calculate_sum(a, b):
+    return a + b
+
+def main():
+    a = 3
+    b = 5
+    result = calculate_sum(a, b)
+    print("The sum of", a, "and", b, "is:", result)
+
+if __name__ == "__main__":
+    main()

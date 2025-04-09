2a6b67a96e7e839a01e70c226d0026d37586bacc
stadnikwhitmeyer529 <stadnikwhitmeyer529@gmail.com>
2025-04-09 15:39:25 +0000

    Add 27.go

---
+++ src/27.go
@@ -0,0 +1,14 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+        return quick_sort(left) + middle + quick_sort(right)
+
+# Example usage
+numbers = [9, 4, 2, 6, 8, 5, 1, 7]
+sorted_numbers = quick_sort(numbers)
+print(sorted_numbers)
